5a550e74c1cab97b2311b980dc2e3103e83d69c6
Jun Zhang <bigonez@gmail.com>
2023-07-22 06:10:29 +1000

    Convert indentation to spaces.
    . change the code indent style, src/**/*.js.
      . convert all indentation to 4 spaces.

--- src/data/cpinfo.js
+++ src/data/cpinfo.js
@@ -1,18 +1,26 @@
-const cpNos = ["Start", "Scenic World", "Furber Pass", "CP1", "Duncan Pass", "CP2",
-               "Ironpot", "CP3", "CP4", "CP5",
-               "CP6", "CP7", "Furber Steps", "BoardWalk", "Finish"];
+const cpNos = [
+    "Start", "Scenic World", "Furber Pass", "CP1", "Duncan Pass", "CP2",
+     "Ironpot", "CP3", "CP4", "CP5",
+     "CP6", "CP7", "Furber Steps", "BoardWalk", "Finish"
+];
 
-const cpNames = ["Start", "Scenic World", "Furber Pass", "Golden Stairs", "Duncan Pass", "Foggy Knob",
-                 "Ironpot Ridge Turnaround", "Six Foot Track", "Katoomba Aquatic Centre", "Fairmont Resort",
-                 "Queen Victoria Hospital", "Sewage Treatment Works", "Base of Furber Steps", "BoardWalk", "Scenic World"];
+const cpNames = [
+    "Start", "Scenic World", "Furber Pass", "Golden Stairs", "Duncan Pass", "Foggy Knob",
+    "Ironpot Ridge Turnaround", "Six Foot Track", "Katoomba Aquatic Centre", "Fairmont Resort",
+    "Queen Victoria Hospital", "Sewage Treatment Works", "Base of Furber Steps", "BoardWalk", "Scenic World"
+];
 
-const cpOdos = [0.0, 4.1, 6.2, 10.1, 21.2, 31.2, 33.6, 44.7, 55.7, 69.1, 78.1, 94.1, 98.9, 99.8, 100.1];
+const cpOdos = [
+    0.0, 4.1, 6.2, 10.1, 21.2, 31.2, 33.6, 44.7, 55.7, 69.1, 78.1, 94.1, 98.9, 99.8, 100.1
+];
 
-const cutOffStrs = ["", "", "", "10:22", "", "15:00", "", "18:50", "22:00", "02:35", "05:45", "", "", "", "11:54"];
+const cutOffStrs = [
+    "", "", "", "10:22", "", "15:00", "", "18:50", "22:00", "02:35", "05:45", "", "", "", "11:54"
+];
 
 export {
-  cpNos,
-  cpNames,
-  cpOdos,
-  cutOffStrs
+    cpNos,
+    cpNames,
+    cpOdos,
+    cutOffStrs
 }
